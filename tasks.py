bbbb795728303e185ec0cb509ad183f5665c27f8
Lasse Duuni <lasse.raty@digitalworkforce.com>
2023-10-10 11:23:14 +0300

    Log API exceptions

--- tasks.py
+++ tasks.py
@@ -48,18 +48,22 @@ def consume_traffic_data():
 def process_traffic_data():
     for item in workitems.inputs:
         traffic_data = item.payload["traffic_data"]
-        valid = validate_traffic_data(traffic_data)
-        if valid:
-            post_traffic_data_to_sales_system(traffic_data)
-
-
-def validate_traffic_data(traffic_data):
-    return len(traffic_data["country"]) == 3
+        if len(traffic_data["country"]) == 3:
+            status, return_json = post_traffic_data_to_sales_system(traffic_data)
+            if status == 200:
+                item.done()
+            else:
+                item.fail(
+                    exception_type="APPLICATION",
+                    code="TRAFFIC_DATA_POST_FAILED",
+                    message=return_json["message"]
+                )
 
 
 def post_traffic_data_to_sales_system(traffic_data):
     url = "https://robocorp.com/inhuman-insurance-inc/sales-system-api"
     response = requests.post(url, json=traffic_data)
+    return response.status_code, response.json()
 
 
 def load_traffic_data_as_table():
